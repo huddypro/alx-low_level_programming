0bc2884f9cd99e5e6b34812e18a3d3bacbf437f3
huddypro <mymail@gmail.com>
2023-02-16 13:27:49 -0800

    Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    // Use the puts function to print the string
+    puts("\"Programming is like building a multilingual puzzle");
+
+    // Exit the program with the value 0
+    return 0;
+}
+
