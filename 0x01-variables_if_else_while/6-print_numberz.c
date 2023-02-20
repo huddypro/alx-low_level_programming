2e95c11e8839faa694cae959061087af443965e1
huddypro <mymail@gmail.com>
2023-02-20 13:51:25 -0800

    Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry code
+ *
+ * Return: 0 Always
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
