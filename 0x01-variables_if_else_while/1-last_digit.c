4b4035024e80c21aab978eb3bf4b1e56be2e490a
huddypro <mymail@gmail.com>
2023-02-20 08:49:01 -0800

    The last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,30 +1,18 @@
 #include <stdlib.h>
-#include <time.h>
 #include <stdio.h>
+#include <time.h>
 /**
- * main - Prrints the last digit of a randomly generated number
- * and weather is greater than 5, less than 6 or 0
+ * main - Entry code
  *
- * Return: Always 0.
+ * Return: 0 Awalys
 */
 int main(void)
 {
-	int n;
+	int n, l_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	if ((n % 10) > 5)
-	{
-		printf("Last digit of %d is %d and is grater than 5\n", n, n % 10);
-	}
-	else if ((n % 10) < 6 && (n % 10) != 0)
-	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n" n, n % 10);
-	}
-	else
-	{
-		printf("Last digit of %d is %d and is 0\n" n, n % 10);
-	}
-
+	l_digit = (n % 10);
+	printf("the last digit of %d is %d", n, l_digit);
 	return (0);
 }
